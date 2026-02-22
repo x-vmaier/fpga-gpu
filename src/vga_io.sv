75cf895585b053c1f736233d652fed5c1fec812d
x-vmaier <valentin16.maier@gmail.com>
2026-02-22 18:57:04 +0100

    Parameterize color depth

--- src/vga_io.sv
+++ src/vga_io.sv
@@ -1,13 +1,13 @@
 `timescale 1ns / 1ps
 
-interface vga_io (
-    input logic clk,
-    input logic rst_n
+interface vga_io #(
+    parameter int R_BITS = 4,
+    parameter int G_BITS = 4,
+    parameter int B_BITS = 4
 );
     logic Hsync;
     logic Vsync;
-    logic [3:0] vgaRed;
-    logic [3:0] vgaGreen;
-    logic [3:0] vgaBlue;
-
+    logic [R_BITS-1:0] vgaRed;
+    logic [G_BITS-1:0] vgaGreen;
+    logic [B_BITS-1:0] vgaBlue;
 endinterface
