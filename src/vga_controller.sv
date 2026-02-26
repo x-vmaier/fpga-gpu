5a364adc735a18048ee295ca231f9620338cb2ab
x-vmaier <valentin16.maier@gmail.com>
2026-02-27 00:13:03 +0100

    Implement framebuffer and upscaling

--- src/vga_controller.sv
+++ src/vga_controller.sv
@@ -2,31 +2,35 @@
 
 (* keep_hierarchy = "yes" *) module vga_controller #(
     parameter int H_ACTIVE = 640,
-    parameter int H_FP = 16,
-    parameter int H_SYNC = 96,
-    parameter int H_BP = 48,
     parameter int V_ACTIVE = 480,
-    parameter int V_FP = 10,
-    parameter int V_SYNC = 2,
-    parameter int V_BP = 33
+    parameter int H_FP     = 16,
+    parameter int H_SYNC   = 96,
+    parameter int H_BP     = 48,
+    parameter int V_FP     = 10,
+    parameter int V_SYNC   = 2,
+    parameter int V_BP     = 33
 ) (
     input logic clk,
     input logic rst_n,
+    input logic [11:0] pixel_data,
+    output logic [$clog2(H_ACTIVE)-1:0] x_dest,
+    output logic [$clog2(V_ACTIVE)-1:0] y_dest,
     output logic [3:0] vgaRed,
     output logic [3:0] vgaGreen,
     output logic [3:0] vgaBlue,
     output logic Hsync,
     output logic Vsync
 );
-
     localparam int H_TOTAL = H_SYNC + H_BP + H_ACTIVE + H_FP;
     localparam int V_TOTAL = V_SYNC + V_BP + V_ACTIVE + V_FP;
 
     logic [$clog2(H_TOTAL)-1:0] Hcnt;
     logic [$clog2(V_TOTAL)-1:0] Vcnt;
     logic display_active;
+    logic display_active_r;
+    logic display_active_rr;
+    logic Hsync_r, Vsync_r;
 
-    // Counters
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             Hcnt <= '0;
@@ -41,38 +45,49 @@
         end
     end
 
+    assign display_active = (Hcnt >= H_SYNC + H_BP && Hcnt < H_SYNC + H_BP + H_ACTIVE) &&
+                            (Vcnt >= V_SYNC + V_BP && Vcnt < V_SYNC + V_BP + V_ACTIVE);
+
+    // Register coordinates and sync signals
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            x_dest <= '0;
+            y_dest <= '0;
+            Hsync_r <= 1'b1;
+            Vsync_r <= 1'b1;
+            display_active_r <= 1'b0;
+        end else begin
+            x_dest <= display_active ? Hcnt - (H_SYNC + H_BP) : '0;
+            y_dest <= display_active ? Vcnt - (V_SYNC + V_BP) : '0;
+            Hsync_r <= ~(Hcnt >= H_ACTIVE + H_FP && Hcnt < H_ACTIVE + H_FP + H_SYNC);
+            Vsync_r <= ~(Vcnt >= V_ACTIVE + V_FP && Vcnt < V_ACTIVE + V_FP + V_SYNC);
+            display_active_r <= display_active;
+        end
+    end
+
+    // Align Hsync/Vsync with BRAM read latency
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             Hsync <= 1'b1;
             Vsync <= 1'b1;
-            display_active <= 1'b0;
+            display_active_rr <= 1'b0;
         end else begin
-            Hsync <= ~(Hcnt >= H_ACTIVE + H_FP && Hcnt < H_ACTIVE + H_FP + H_SYNC);
-            Vsync <= ~(Vcnt >= V_ACTIVE + V_FP && Vcnt < V_ACTIVE + V_FP + V_SYNC);
-
-            display_active <= (Hcnt >= H_SYNC + H_BP && Hcnt < H_SYNC + H_BP + H_ACTIVE) &&
-                              (Vcnt >= V_SYNC + V_BP && Vcnt < V_SYNC + V_BP + V_ACTIVE);
+            Hsync <= Hsync_r;
+            Vsync <= Vsync_r;
+            display_active_rr <= display_active_r;
         end
     end
 
-    // Pixel output
+    // RGB output
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             vgaRed   <= 4'h0;
             vgaGreen <= 4'h0;
             vgaBlue  <= 4'h0;
         end else begin
-            if (display_active) begin
-                vgaRed   <= 4'h0;
-                vgaGreen <= 4'hF;
-                vgaBlue  <= 4'hF;
-            end else begin
-                // Drive black outside active region
-                vgaRed   <= 4'h0;
-                vgaGreen <= 4'h0;
-                vgaBlue  <= 4'h0;
-            end
+            vgaRed   <= display_active_rr ? pixel_data[11:8] : 4'h0;
+            vgaGreen <= display_active_rr ? pixel_data[7:4] : 4'h0;
+            vgaBlue  <= display_active_rr ? pixel_data[3:0] : 4'h0;
         end
     end
-
 endmodule
