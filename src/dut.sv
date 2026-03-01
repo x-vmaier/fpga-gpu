b007a6f754806beca3cecf938462808410db7094
x-vmaier <valentin16.maier@gmail.com>
2026-03-01 19:51:23 +0100

    Utilize input synchronization

--- src/dut.sv
+++ src/dut.sv
@@ -26,11 +26,16 @@
     logic rst_n_clk_vga;
     logic pll_locked;
 
+    logic rx_sync;
+    logic [15:0] sw_sync;
+    logic [16:0] async_in;
+    logic [16:0] sync_out;
+
     logic [15:0] segment_data_in;
     logic [3:0] segment_point_in;
 
     assign segment_point_in = '0;
-    assign LED = sw;
+    assign LED = sw_sync;
 
     // Clock wizard (MMCM)
     clk_wiz_0 cw0 (
@@ -54,29 +59,53 @@
     assign rst_n_clk_osc = rst_arr[DOM_SYS];
     assign rst_n_clk_vga = rst_arr[DOM_VGA];
 
+    // Input synchronization
+    assign async_in = {sw, RsRx};
+
+    input_sync #(
+        .NUM_SIGNALS(17),
+        .SYNC_STAGES(2)
+    ) input_sync0 (
+        .clk     (clk_osc),
+        .rst_n   (rst_n_clk_osc),
+        .async_in(async_in),
+        .sync_out(sync_out)
+    );
+
+    assign rx_sync = sync_out[0];
+    assign sw_sync = sync_out[16:1];
+
     // UART interface
     uart_io uart_if (
         .clk  (clk_osc),
         .rst_n(rst_n_clk_osc)
     );
 
-    assign uart_if.rx = RsRx;
+    assign uart_if.rx = rx_sync;
     assign RsTx = uart_if.tx;
-    
-    assign segment_data_in = {4'b0000, 4'b0000, uart_if.rx_data[7:4], uart_if.rx_data[3:0]};
+
+    assign segment_data_in = {8'b0, uart_if.rx_data};
 
     // Sub-blocks
     uart u_uart0 (.uart_if(uart_if));
 
     seven_segment_translator sst0 (
-        .clk  (clk_osc),
-        .rst_n(rst_n_clk_osc),
-        .*
+        .clk             (clk_osc),
+        .rst_n           (rst_n_clk_osc),
+        .segment_data_in (segment_data_in),
+        .segment_point_in(segment_point_in),
+        .seg             (seg),
+        .an              (an),
+        .dp              (dp)
     );
 
     display_engine de0 (
-        .clk  (clk_vga),
-        .rst_n(rst_n_clk_vga),
-        .*
+        .clk     (clk_vga),
+        .rst_n   (rst_n_clk_vga),
+        .vgaRed  (vgaRed),
+        .vgaGreen(vgaGreen),
+        .vgaBlue (vgaBlue),
+        .Hsync   (Hsync),
+        .Vsync   (Vsync)
     );
 endmodule
