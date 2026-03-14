1b4b848b40441a39b499d282a82a22fc906c1914
x-vmaier <valentin16.maier@gmail.com>
2026-03-14 23:20:06 +0100

    Calculate Rx valid timeout

--- sim/tb_uart.sv
+++ sim/tb_uart.sv
@@ -4,7 +4,11 @@
 `include "TB_macro_collection.vh"
 
 program tb_uart ();
-    parameter realtime BAUD_PERIOD = 1_000_000_000.0 / 921_600.0;
+    parameter realtime BAUD_PERIOD  = 1_000_000_000.0 / 921_600.0;
+    parameter int DATA_BITS         = 8;
+    parameter int STOP_BITS         = 1;
+    parameter int TEST_MARGIN       = 1;
+    parameter realtime FRAME_PERIOD = (1 + DATA_BITS + STOP_BITS + TEST_MARGIN) * BAUD_PERIOD;
 
     byte test_byte;
 
@@ -17,9 +21,9 @@ program tb_uart ();
             test_byte = $urandom_range(0, 8'hFF);
             fork
                 uart_send_byte(test_byte, BAUD_PERIOD, TB.RsRx);
-                `wait_for_posedge(TB.dut0.uart_if.rx_valid, 100us)
+                `wait_for_posedge(TB.u_dut.uart_if.rx_valid, FRAME_PERIOD)
             join
-            `Check(TB.dut0.uart_if.rx_data == test_byte, ("Byte %0d: expected 0x%02X got 0x%02X", i, test_byte, TB.dut0.uart_if.rx_data))
+            `Check(TB.u_dut.uart_if.rx_data == test_byte, ("Byte %0d: expected 0x%02X got 0x%02X", i, test_byte, TB.u_dut.uart_if.rx_data))
         end
 
     `TB_TEST_END(15ms)
